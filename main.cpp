7e050ff509214efd69b4d96273aaa41e016db6d5
Ivan <146617492+ivannZoros@users.noreply.github.com>
2023-12-02 09:12:57 +0600

    function

--- main.cpp
+++ main.cpp
@@ -5,53 +5,48 @@
 #include <time.h>
 #include <algorithm>
 using namespace std;
-void ranndword() {
-    vector <string> words = {"apples","banana","viseliza","dogs","rogaliks"};
-    srand(time(0));
-    int num = rand() % 5;
+string shufle(const string& words) {
+    string shuffle_word = words;
+    shuffle(shuffle_word.begin(),shuffle_word.end(), std::mt19937(std::random_device()()));
+    return shuffle_word;
+}
+
+bool check_answer(const string& correct_answer, const string& ans) {
+    return correct_answer == ans;
 }
 int main() {
-    int flag = 0;
-     vector <string> words = {"apples","banana","viseliza","dogs","rogaliks"};
     srand(time(0));
-    int num = rand() % 5;
-    string word = words[num];
-    shuffle(words[num].begin(),words[num].end(), std::mt19937(std::random_device()()));
     int lives = 3;
+    int help = 3;
+    vector<string> words = {"apples", "banana", "viseliza", "dogs", "rogaliks"};
+    int numwords = words.size();
+    cout << "Your lives - " << lives << endl;
+    cout << "You can use - " << help << "  help" << endl;
     string ans;
-    bool isgameover = false;
-    while(!isgameover) {
-        cout << "The words is - " <<word << endl;
-        cout << "Your lives - " << lives << endl;
-        cout << "Enter answer" << endl;
-        cout << words[num] << endl;
+    for (int i = 0; i < numwords; i++) {
+        string correct_word = words[i];
+        string shuffle_word = shufle(correct_word);
+        cout << "The words is - " << correct_word << endl;
         cin >> ans;
-        if (ans == "help"){
-            string help = words[num].substr(0, words[num].size() / 2);
-            cout << "1/2 of words is - " << help << endl;
-            flag++;
-        }
-        if (ans == "help" && flag == 4){
-            cout << "Only 3 help!" << endl;
-        }
-        if (ans == words[num]) {
-            cout << "Win!!Contuinue? - y/n" << endl;
-            string cont;
-            if (cont == "y"){
-                num = rand() % 5;
-            }else if(cont == "n") {
-                isgameover = true;
+        if (check_answer(correct_word, ans)) {
+            cout << "WIN!" << endl;
+            if (i == numwords - 1) {
+                cout << "The words have been shuffled" << endl;
+                shuffle(words.begin(), words.end(), std::mt19937(std::random_device()()));
             }
-        } else if (ans != "help" || ans != "y" ) {
+        } else if (ans == "help" && help > 0) {
+            cout << "Help -  " << correct_word.substr(0, correct_word.length() / 2) << endl;
+            help--;
+            i--;
+        } else {
+            cout << "Wrong" << endl;
             lives--;
-            cout << "wrong" << endl;
-        }
-        if (lives == 0){
-            cout << "GAME OVER" << endl;
-            break;
+            if (lives == 0) {
+                cout << "GAME OVER" << endl;
+                return 0;
+            }
         }
     }
 
+
 }
-
-
