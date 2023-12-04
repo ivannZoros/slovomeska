fe726a39e38405f8b0e298268d0c2f55b29f9894
Ivan <146617492+ivannZoros@users.noreply.github.com>
2023-12-04 18:33:34 +0600

    function

--- main.cpp
+++ main.cpp
@@ -11,36 +11,46 @@ string shufle(const string& words) {
     return shuffle_word;
 }
 
-bool check_answer(const string& correct_answer, const string& ans) {
-    return correct_answer == ans;
+bool check_answer(const string& shuffle_word, const string& ans) {
+    if (shuffle_word == ans){
+        return true;
+    }
+    return false;
 }
 int main() {
     srand(time(0));
     int lives = 3;
     int help = 3;
-    vector<string> words = {"apples", "banana", "viseliza", "dogs", "rogaliks"};
+    vector<string> words = {"apple", "banana", "viseliza", "dogs", "rogalik"};
     int numwords = words.size();
-    cout << "Your lives - " << lives << endl;
-    cout << "You can use - " << help << "  help" << endl;
-    string ans;
-    for (int i = 0; i < numwords; i++) {
+    for (int i = 0; i < numwords;) {
         string correct_word = words[i];
         string shuffle_word = shufle(correct_word);
+        cout << "Your lives - " << lives << endl;
+        cout << "You can use - " << help << "  help" << endl;
+        cout << shuffle_word << " - shuffled"<< endl;
         cout << "The words is - " << correct_word << endl;
+        string ans;
         cin >> ans;
-        if (check_answer(correct_word, ans)) {
-            cout << "WIN!" << endl;
+        if (ans == "help" && help > 0) {
+            cout << "Help -  " << shuffle_word.substr(0, shuffle_word.length() / 2) << endl;
+            help--;
+            //--i;
+        }
+        else if (check_answer(shuffle_word, ans)) {
+            cout << "Next!" << endl;
+            i++;
             if (i == numwords - 1) {
-                cout << "The words have been shuffled" << endl;
+                cout << "WIN" << endl;
                 shuffle(words.begin(), words.end(), std::mt19937(std::random_device()()));
             }
-        } else if (ans == "help" && help > 0) {
-            cout << "Help -  " << correct_word.substr(0, correct_word.length() / 2) << endl;
-            help--;
-            i--;
-        } else {
+        } else if (ans == "help" && help == 0) {
+            cout << "All helps have been used" << endl;
+        }
+        else {
             cout << "Wrong" << endl;
             lives--;
+
             if (lives == 0) {
                 cout << "GAME OVER" << endl;
                 return 0;
